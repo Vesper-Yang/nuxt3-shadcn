c0c7c2af3e3fded829c343c038a1f5b9bd809a09
Vesper-Yang <1105061841@qq.com>
2024-08-20 14:59:31 +0800

    将折线图转换成单独的组件

---
+++ components/Chart.vue
@@ -0,0 +1,154 @@
+<script setup lang="ts">
+let props = defineProps(['currentCategory', 'data'])
+let data = props.data || []
+let currentCategory = props.currentCategory || 'today'
+
+interface Categories {
+  today: string[];
+  week: string[];
+  year: string[];
+  month?: string[]; 
+}
+
+let categories = ref<Categories>({
+  today: [
+    "00:00",
+    "01:00",
+    "02:00",
+    "03:00",
+    "04:00",
+    "05:00",
+    "06:00",
+    "07:00",
+    "08:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+    "23:00"
+  ],
+  week: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ],
+  year: [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ]
+})
+
+
+const selectedCategories = computed(() => {
+  return categories.value[currentCategory.value as keyof typeof categories.value];
+});
+
+
+let options = computed(() => ({
+  chart: {
+    type: 'line',
+    animation: {
+      enabled: false
+    }
+  },
+  title: {
+    text: ''
+  },
+  xAxis: {
+    gridLineColor: 'transparent', // remove line
+    categories: selectedCategories.value
+  },
+  yAxis: {
+    gridLineColor: 'transparent',
+    title: { // remove title
+      text: ''
+    }
+  },
+  legend: {
+    enabled: false
+  },
+  plotOptions: {
+    line: {
+      marker: { // remove points
+        enabled: false
+      },
+      dataLabels: {
+        enabled: false
+      },
+      enableMouseTracking: false
+    }
+  },
+  series: [{
+    name: 'line',
+    lineWidth: '4px',
+    color: { // gradients
+      linearGradient: { y1: 0, y2: 0, y3: 0, y4: 0 },
+      stops: [
+        [0, 'rgba(252,176,69,1)'],
+        [0.33, 'rgba(253,29,29,1)'],
+        [0.66, 'rgba(131,58,180,1)'],
+        [1, 'rgba(29,217,93,1)'],
+      ]
+    },
+    data
+  }]
+}))
+
+
+function generateMonth() {
+  let currentDate = new Date();
+  let currentMonth = currentDate.getMonth() + 1;
+  let currentYear = currentDate.getFullYear(); 
+
+  function generateMonthDates() {
+    let monthDates = [];
+    let daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
+
+    for (let i = 1; i <= daysInMonth; i++) {
+      let dayString = ("0" + i).slice(-2); 
+      let monthString = ("0" + currentMonth).slice(-2); 
+      monthDates.push(monthString + "/" + dayString);
+    }
+
+    return monthDates;
+  }
+
+  let month = generateMonthDates();
+  categories.value = { ...categories.value, month }; 
+  return month;
+}
+
+onMounted(() => {
+  generateMonth()
+})
+</script>
+
+<template>
+  <div>
+    <highchart class="h-[300px] border p-4 rounded-lg" v-if="data.length > 0" :options="options" />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-const loading =ref(false)
+import { Label } from 'radix-vue';
 const list = [
   {
     title: "Today",
@@ -22,123 +22,8 @@ const list = [
 let data = ref([16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8, 38.4, 35.5, 29.2, 22.0, 17.8])
 
 
-interface Categories {
-  today: string[];
-  week: string[];
-  year: string[];
-  month?: string[]; // 可选属性 month
-}
-
-
-let categories = ref(<Categories>{
-  today: [
-    "00:00",
-    "01:00",
-    "02:00",
-    "03:00",
-    "04:00",
-    "05:00",
-    "06:00",
-    "07:00",
-    "08:00",
-    "09:00",
-    "10:00",
-    "11:00",
-    "12:00",
-    "13:00",
-    "14:00",
-    "15:00",
-    "16:00",
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-    "21:00",
-    "22:00",
-    "23:00"
-  ],
-  week: [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-  ],
-  year: [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec'
-  ]
-})
 let currentCategory = ref('today')
 
-const selectedCategories = computed(() => {
-  return categories.value[currentCategory.value as keyof typeof categories.value];
-});
-
-const options = computed(() => (
-  {
-    chart: {
-        type: 'line',
-        animation: {
-          enabled: false
-        }
-    },
-    legend: {
-      enabled: false
-    },
-    title: {
-        text: ''
-    },
-    xAxis: {
-        gridLineColor: 'transparent', // remove line
-        categories: selectedCategories.value
-      },
-    yAxis: {
-        gridLineColor: 'transparent',
-        title: {
-            text: ''
-        }
-    },
-    plotOptions: {
-        line: {
-            marker: {
-              enabled: false
-            },
-            dataLabels: {
-                enabled: false
-            },
-            enableMouseTracking: true
-        }
-    },
-    series: [{
-        name: 'line',
-        lineWidth: '4px',
-        color: {
-          linearGradient : {},
-          stops: [
-            [0, 'rgba(252, 176, 69, 1)'],
-            [0.33, 'rgba(253, 29, 29, 1)'],
-            [0.66, 'rgba(131, 58, 180, 1)'],
-            [1, 'rgba(29, 217, 83, 1)'],
-
-          ]
-        },
-        data: data.value
-    }]
-}
-))
 
 const setCategory = (e: { target: { innerText: string; }; }) => {
   let v = e.target.innerText.toLowerCase()
@@ -172,38 +57,37 @@ function generateRandomData(number = 7) {
   return values
 }
 
-function generateMonth() {
-  let currentDate = new Date();
-  let currentMonth = currentDate.getMonth() + 1;
-  let currentYear = currentDate.getFullYear(); 
-
-  function generateMonthDates() {
-    let monthDates = [];
-    let daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
-
-    for (let i = 1; i <= daysInMonth; i++) {
-      let dayString = ("0" + i).slice(-2); 
-      let monthString = ("0" + currentMonth).slice(-2); 
-      monthDates.push(monthString + "/" + dayString);
-    }
-
-    return monthDates;
+const cards = [
+  {
+    title: "Sales",
+    progression: 12,
+    amount: 84.44,
+    Label: "View sales",
+    description: "Sales of March 2024",
+    icon: "solar: ticket-sale-outline"
+  },
+  {
+    title: "Sales",
+    progression: 12,
+    amount: 84.44
+  },
+  {
+    title: "Sales",
+    progression: 12,
+    amount: 84.44
   }
-
-  let month = generateMonthDates();
-  categories.value = { ...categories.value, month }; 
-  return month;
-}
+]
 
 onMounted(() => {
-  generateRandomData()
-  generateMonth()
+  generateRandomData(24)
 })
+
+
 
 </script>
 
 <template>
-  <div class="grid gap-4 w-full">
+  <div class="grid gap-4">
     <header class="flex items-start justify-between">
       <div class="grow">
         <p>Hi, welcome back Bunan!</p>
@@ -219,16 +103,16 @@ onMounted(() => {
           </TabsTrigger>
         </TabsList>
         <TabsContent v-for="(item, index) in list" :key="index" :value="item.title">
-          <highchart v-if="data.length > 0" :options="options" />
+          <Chart v-if="data.length > 0" :currentCategory="currentCategory" :data="data" />
         </TabsContent>
       </Tabs>
-      <section class=" bg-neutral-200 w-full h-[300px]">
+      <section class=" bg-neutral-200 w-full h-[150px]">
         Chart
       </section>
     </main>
     <footer>
       <div class="flex ietms-center gap-4">
-        <div v-for="(item, index) in 3" :key="index" class=" bg-neutral-200 w-full h-[300px]">I'll have 3 differents cards.</div>
+        <div v-for="(item, index) in 3" :key="index" class=" bg-neutral-200 w-full h-[100px]">I'll have 3 differents cards.</div>
       </div>
     </footer>
   </div>
